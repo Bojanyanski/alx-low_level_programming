26fb3cd2c0317c4857d817711adc3579fdb16246
Bojanyanski <morganseth09@gmail.com>
2022-09-08 09:04:06 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,12 @@
 /**
  * main - print using the printf 
  * 
+ * Description: using the main fuction
+ * this program prints "With proper grammar, but the outcome is a piece of art"
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return 0;
 }
