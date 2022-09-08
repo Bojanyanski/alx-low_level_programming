cc43eff91c88ff9b8b9f3db730690fa0d606a872
Bojanyanski <morganseth09@gmail.com>
2022-09-08 08:28:14 -0700

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print using the puts function
+ *
+ * narration :using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
