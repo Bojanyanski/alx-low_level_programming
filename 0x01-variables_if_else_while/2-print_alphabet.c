b01bb08683e73cafdd01391c4d1b9afcef2749ca
Bojanyanski <morganseth09@gmail.com>
2022-09-11 07:14:47 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet game
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch
+		;
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+
+	putchar(ch);
+
+	return (0);
+}
