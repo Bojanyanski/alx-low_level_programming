b6f93f100f317b7923010e434b74df25610ae985
Bojanyanski <morganseth09@gmail.com>
2022-09-12 07:13:03 -0700

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Reverse
+ *
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n > 'a'; n--)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
