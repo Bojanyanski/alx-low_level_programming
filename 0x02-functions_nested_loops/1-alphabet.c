6f846f7c0b7f7fa1d7120af4ef62e3ebe765327c
Bojanyanski <morganseth09@gmail.com>
2022-09-13 15:48:06 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Alphbets
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int u;
+
+	for (u = 'a'; u <= 'z'; u++)
+	{
+		_putchar(u);
+	}
+	putchar('\n');
+
+}
