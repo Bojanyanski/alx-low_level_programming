f82a66aecd67197e7f8eb18d2f447ea618f8a98d
Bojanyanski <morganseth09@gmail.com>
2022-09-14 06:16:00 -0700

    Is_lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower: lower alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int k;
+
+	if (k >= 'a' && k <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
