7898015e866de7f5f82f4935319f45941e59f71f
Bojanyanski <morganseth09@gmail.com>
2022-09-14 18:02:30 -0700

    Natutral numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_to_98 - unction that prints all natural number
+ * @n: starting point
+ * Return: Always 0 (Sucess)
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar("%d", n);
+	}
+	_putchar('\n');
+}
