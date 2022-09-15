124fb8320355b2aa0948b3a74fc915770f8d4c01
Bojanyanski <morganseth09@gmail.com>
2022-09-14 17:42:32 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - a function that adds two integers and returns the result
- * @x - parameter1
- * @y - parameter2
+ * @x: parameter1
+ * @y: parameter2
  * Return: Always 0 (Success)
  */
 int add(int x, int y)
