fa44fdc1434588d1a3515e21e0d5180c8ea6bc85
Bojanyanski <morganseth09@gmail.com>
2022-09-27 13:18:36 -0700

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,12 +12,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i = 0;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+
+	if (*(s + i) == c)
 		return (s + i);
-	else
-		return (NULL);
+	return ('\0');
 }
